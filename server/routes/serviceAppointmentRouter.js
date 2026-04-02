dcf6280fef8a19074c23e5d6f2fc849cea56b647
Arman Ali <armanali0178614@gmail.com>
2026-04-02 21:10:18 +0530

    Backend Done

--- server/routes/serviceAppointmentRouter.js
+++ server/routes/serviceAppointmentRouter.js
@@ -2,9 +2,38 @@ import express from "express";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 import {
-  getAllServiceAppointments,
-  getServiceAppointmentById,
+  cancelServiceAppointment,
+  confirmServicePayment,
   createServiceAppointment,
+  getServiceAppointmentById,
+  getServiceAppointments,
+  getServiceAppointmentsByPatient,
+  getServiceAppointmentStats,
   updateServiceAppointment,
-  deleteServiceAppointment,
 } from "../controllers/serviceAppointmentController.js";
+
+const serviceAppointmentRouter = express.Router();
+
+serviceAppointmentRouter.get("/", getServiceAppointments);
+serviceAppointmentRouter.get("/confirm", confirmServicePayment);
+serviceAppointmentRouter.get("/stats/summary", getServiceAppointmentStats);
+
+serviceAppointmentRouter.post(
+  "/",
+  clerkMiddleware(),
+  requireAuth(),
+  createServiceAppointment,
+);
+
+serviceAppointmentRouter.get(
+  "/me",
+  clerkMiddleware(),
+  requireAuth(),
+  getServiceAppointmentsByPatient,
+);
+
+serviceAppointmentRouter.get("/:id", getServiceAppointmentById);
+serviceAppointmentRouter.put("/:id", updateServiceAppointment);
+serviceAppointmentRouter.post("/:id/cancel", cancelServiceAppointment);
+
+export default serviceAppointmentRouter;
